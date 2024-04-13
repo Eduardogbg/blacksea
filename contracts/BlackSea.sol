beaa1559e23ff7273708b4df7a31740b8f8f1330
Jasso <veigajoaoa@gmail.com>
2024-04-13 17:50:43 -0300

    add base settlement engine

---
+++ contracts/BlackSea.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+import "./BokkyPooBahsRedBlackTreeLibrary.sol";
+
+contract BlackSea {
+    constructor() public {}
+
+    function placeOrder() external {
+
+    }
+
+    function cancelOrder() external {
+
+    }
+
+    function
+    
+}
